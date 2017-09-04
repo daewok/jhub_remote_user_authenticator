6fc82492d45499217f9dc58bd15035c88130e4f1
Eric Timmons <etimmons@mit.edu>
2017-09-04 12:56:34 -0400

    Add user_suffix and remove_user_suffix options.

--- remote_user/remote_user_auth.py
+++ remote_user/remote_user_auth.py
@@ -5,7 +5,7 @@ from jupyterhub.auth import Authenticator
 from jupyterhub.auth import LocalAuthenticator
 from jupyterhub.utils import url_path_join
 from tornado import gen, web
-from traitlets import Unicode
+from traitlets import Unicode, Bool
 
 
 class RemoteUserLoginHandler(BaseHandler):
@@ -13,9 +13,16 @@ class RemoteUserLoginHandler(BaseHandler):
     def get(self):
         header_name = self.authenticator.header_name
         remote_user = self.request.headers.get(header_name, "")
-        if remote_user == "":
+        user_suffix = self.authenticator.user_suffix
+        remove_user_suffix = self.authenticator.remove_user_suffix
+
+        if len(remote_user) <= len(user_suffix) \
+           or not remote_user.endswith(user_suffix):
             raise web.HTTPError(401)
         else:
+            if remove_user_suffix and not user_suffix == '':
+                remote_user = remote_user[:-len(user_suffix)]
+
             user = self.user_from_username(remote_user)
             self.set_login_cookie(user)
             self.redirect(url_path_join(self.hub.server.base_url, 'home'))
@@ -29,6 +36,16 @@ class RemoteUserAuthenticator(Authenticator):
         default_value='REMOTE_USER',
         config=True,
         help="""HTTP header to inspect for the authenticated username.""")
+
+    user_suffix = Unicode(
+        default_value='',
+        config=True,
+        help="""Ensure that all usernames have this suffix.""")
+
+    remove_user_suffix = Bool(
+        default_value=False,
+        config=True,
+        help="""Should the user_suffix be removed.""")
 
     def get_handlers(self, app):
         return [
@@ -50,6 +67,16 @@ class RemoteUserLocalAuthenticator(LocalAuthenticator):
         default_value='REMOTE_USER',
         config=True,
         help="""HTTP header to inspect for the authenticated username.""")
+
+    user_suffix = Unicode(
+        default_value='',
+        config=True,
+        help="""Ensure that all usernames have this suffix.""")
+
+    remove_user_suffix = Bool(
+        default_value=False,
+        config=True,
+        help="""Should the user_suffix be removed.""")
 
     def get_handlers(self, app):
         return [
